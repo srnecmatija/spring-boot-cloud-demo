ee5c81c589880b4a8fceca60f33d5ff9d585756f
srnecmatija <srnec.matija@gmail.com>
2025-10-15 11:32:55 +0200

    - added a config client to name service to read the configuration from the config server

--- name/src/main/java/com/ibmix/springcloud/demo/name/NameController.java
+++ name/src/main/java/com/ibmix/springcloud/demo/name/NameController.java
@@ -3,8 +3,12 @@ package com.ibmix.springcloud.demo.name;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class NameController {
+
+    private static final Logger LOG = Logger.getLogger(NameController.class.getName());
 
     private final NameProperties nameProperties;
 
@@ -14,6 +18,7 @@ public class NameController {
 
     @RequestMapping
     public String getName() {
+        LOG.info("Name: " + this.nameProperties.getName());
         return nameProperties.getName();
     }
 }
