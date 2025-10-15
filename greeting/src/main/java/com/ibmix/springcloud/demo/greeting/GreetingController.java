372ebbbd7089e2822a5ef31819628b904fdc846f
srnecmatija <srnec.matija@gmail.com>
2025-10-15 10:30:13 +0200

    - config server adjustements - added a config client to greeting service to read the configuration from the config server

--- greeting/src/main/java/com/ibmix/springcloud/demo/greeting/GreetingController.java
+++ greeting/src/main/java/com/ibmix/springcloud/demo/greeting/GreetingController.java
@@ -4,8 +4,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class GreetingController {
+
+    private static final Logger LOG = Logger.getLogger(GreetingController.class.getName());
 
     private final GreetingProperties greetingProperties;
 
@@ -15,11 +19,14 @@ public class GreetingController {
 
     @RequestMapping("/")
     public String getGreeting() {
+        LOG.info("Greeting: " + greetingProperties.getGreeting());
         return this.greetingProperties.getGreeting();
     }
 
     @RequestMapping("/{languageCode}")
     public String getGreeting(@PathVariable String languageCode) {
+        LOG.info("Language Code: " + languageCode);
+        LOG.info("Greeting: " + this.greetingProperties.getGreetings().get(languageCode.toUpperCase()));
         return  this.greetingProperties.getGreetings().getOrDefault(languageCode.toUpperCase(), this.greetingProperties.getGreeting());
     }
 }
