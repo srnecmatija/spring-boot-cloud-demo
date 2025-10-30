e405e1f2d62dbcd2bc6566c4e0242f0085c4257d
srnecmatija <srnec.matija@gmail.com>
2025-10-30 10:44:20 +0100

    added service discovery client to web app

--- web/src/main/java/com/ibmix/springcloud/demo/web/WebApplication.java
+++ web/src/main/java/com/ibmix/springcloud/demo/web/WebApplication.java
@@ -3,11 +3,13 @@ package com.ibmix.springcloud.demo.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebApplication {
 
 	public static void main(String[] args) {
