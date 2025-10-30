e54842b450febda6b6cef0b9c0d28b4b0b6372e5
srnecmatija <srnec.matija@gmail.com>
2025-10-30 11:02:42 +0100

    web app refactoring using service discovery to use the services instead of having hardcoded urls

--- web/src/main/java/com/ibmix/springcloud/demo/web/WebApplication.java
+++ web/src/main/java/com/ibmix/springcloud/demo/web/WebApplication.java
@@ -2,11 +2,7 @@ package com.ibmix.springcloud.demo.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,15 +10,6 @@ public class WebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
-	}
-
-	@Configuration
-	static class WebConfig {
-
-		@Bean
-		public RestTemplate restTemplate() {
-			return new RestTemplateBuilder().build();
-		}
 	}
 
 }
--- web/src/main/java/com/ibmix/springcloud/demo/web/WebController.java
+++ web/src/main/java/com/ibmix/springcloud/demo/web/WebController.java
@@ -1,29 +1,43 @@
 package com.ibmix.springcloud.demo.web;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 @RestController
 public class WebController {
 
-    private final NameService nameService;
-    private final GreetingService greetingService;
+    private final DiscoveryClient discoveryClient;
+    private final RestClient restClient;
 
-    public WebController(NameService nameService, GreetingService greetingService) {
-        this.nameService = nameService;
-        this.greetingService = greetingService;
+    public WebController(DiscoveryClient discoveryClient, RestClient.Builder restClientBuilder) {
+        this.discoveryClient = discoveryClient;
+        this.restClient = restClientBuilder.build();
     }
 
     @RequestMapping
     public String getResult(HttpServletRequest request) {
         final String locale = RequestContextUtils.getLocaleResolver(request).resolveLocale(request).toLanguageTag();
 
-        return new StringBuilder()
-                .append(this.greetingService.getGreeting(locale))
-                .append(" ")
-                .append(this.nameService.getName())
-                .toString();
+        final ServiceInstance greetingService = discoveryClient.getInstances("greeting").get(0);
+        final ServiceInstance nameService = discoveryClient.getInstances("name").get(0);
+
+        final String greetingServiceResponse = this.restClient
+                .get()
+                .uri(greetingService.getUri() + "/" + locale)
+                .retrieve()
+                .body(String.class);
+
+        final String nameServiceResponse = this.restClient
+                .get()
+                .uri(nameService.getUri())
+                .retrieve()
+                .body(String.class);
+
+        return greetingServiceResponse + " " + nameServiceResponse;
     }
 }
