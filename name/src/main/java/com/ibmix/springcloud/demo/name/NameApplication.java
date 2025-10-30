f71aa5cf75aa915fc4d29f7edd25159283845220
srnecmatija <srnec.matija@gmail.com>
2025-10-30 10:33:56 +0100

    added service discovery client to name and greeting services

--- name/src/main/java/com/ibmix/springcloud/demo/name/NameApplication.java
+++ name/src/main/java/com/ibmix/springcloud/demo/name/NameApplication.java
@@ -3,9 +3,11 @@ package com.ibmix.springcloud.demo.name;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties(NameProperties.class)
+@EnableDiscoveryClient
 public class NameApplication {
 
 	public static void main(String[] args) {
