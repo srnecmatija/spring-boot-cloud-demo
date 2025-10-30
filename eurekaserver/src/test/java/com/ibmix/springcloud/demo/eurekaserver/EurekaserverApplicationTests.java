df729a6b4cf49525645993751e19d8cac3ec428d
srnecmatija <srnec.matija@gmail.com>
2025-10-30 10:04:18 +0100

    created service discovery (eureka) server app

---
+++ eurekaserver/src/test/java/com/ibmix/springcloud/demo/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibmix.springcloud.demo.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
