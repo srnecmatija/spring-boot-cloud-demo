9761cec034eca68ad481792b5fc6a3b8fff5734e
srnecmatija <srnec.matija@gmail.com>
2025-11-11 10:52:15 +0100

    - created api gateway project - added api routes

---
+++ apigatewayserver/src/main/java/com/ibmix/springcloud/demo/apigatewayserver/ApigatewayserverApplication.java
@@ -0,0 +1,13 @@
+package com.ibmix.springcloud.demo.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayserverApplication.class, args);
+	}
+
+}
---
+++ apigatewayserver/src/test/java/com/ibmix/springcloud/demo/apigatewayserver/ApigatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibmix.springcloud.demo.apigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
